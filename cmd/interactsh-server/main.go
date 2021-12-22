0c8407331752fa91a838565aec9c74500f1f534e
Serizao <11671895+Serizao@users.noreply.github.com>
2021-12-22 20:42:18 +0100

    Update main.go

--- cmd/interactsh-server/main.go
+++ cmd/interactsh-server/main.go
@@ -16,7 +16,7 @@ import (
 
 	"github.com/projectdiscovery/gologger"
 	"github.com/projectdiscovery/gologger/levels"
-	"github.com/projectdiscovery/interactsh/pkg/server"
+	"github.com/Serizao/interactsh3/pkg/server"
 	"github.com/projectdiscovery/interactsh/pkg/server/acme"
 	"github.com/projectdiscovery/interactsh/pkg/storage"
 )
